f177d14f925cb2818db51ff92e08b099255a8a50
Allan Martinez <martinezallan123@gmail.com>
2023-08-16 18:05:39 -0700

    adjust dollar sign placement

--- nonOCRtxtGenerator.py
+++ nonOCRtxtGenerator.py
@@ -19,9 +19,9 @@ def mustardsParser():
             # If the last character is a number string, add a dollar sign and a new line
             int(lastChar)
 
-            # Place the dollar sign 3 characters before the last number string
+            # Place the dollar sign 4 characters before the last number string
             # ex: '1000' -> '$1000'
-            hasDollarSign = line[:-4] + '$' + line[-4:] + "\n"
+            hasDollarSign = line[:-5] + '$' + line[-5:] + "\n"
 
             # Remove any whitespace between the dollar sign and the price
             final += re.sub(r'\$\s*', '$', hasDollarSign)
