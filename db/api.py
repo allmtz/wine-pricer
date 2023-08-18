70461fb8f656aac2ebb566068c7b174a47c19c9e
Allan Martinez <martinezallan123@gmail.com>
2023-08-18 13:56:15 -0700

    allow CORS for all routes

--- db/api.py
+++ db/api.py
@@ -1,7 +1,11 @@
 import flask
 from db_functions import searchAllTables, createConnection
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
+
+# Allow CORS for all routes
+CORS(app)
 
 
 @app.route('/search/all/<s>', methods=['GET'])
